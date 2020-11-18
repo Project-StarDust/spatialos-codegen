8667576f7ca92bc5e77806a63a0b988ff80ddf45
Thomas Nicollet <thomas.nicollet@epitech.eu>
2020-11-18 13:58:02 +0900

    :bug: Changed headers

--- src/ast/header.rs
+++ src/ast/header.rs
@@ -3,8 +3,7 @@ pub struct Header {}
 impl Header {
     pub fn generate() -> String {
         format!(
-            "{}\n{}",
-            "#[allow(unused_imports)]\nuse spatial_macro::spatial_enum;",
+            "{}",
             "#[allow(unused_imports)]\nuse std::collections::HashMap;"
         )
     }
